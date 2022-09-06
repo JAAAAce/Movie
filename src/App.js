28404632826b0a54010d3924beda76fbaddf8d6d
JAAAAce <tksto0915@naver.com>
2022-09-06 11:51:07 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.scss'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './routes/Home';
+import './App.scss';
+import About from './routes/About';
+import Navigation from './components/Navigation';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  } //root 위치
-  getMovies = async () => {
-    const { //구조 분할 할당
-      data: {
-        data: {movies},
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    //console.log(movies) - data.data.movie의 array
-    this.setState({movies, isLoading: false})
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const {isLoading, movies} = this.state;
-    return(
-      <section className='container'>
-        {isLoading ? (<div className='loader'>
-          <span className='loader_text'>'Loading...'</span>
-          </div>) : (
-            <div className='movies'>
-              {movies.map((movie) => (
-                //console.log(movie);
-                <Movie  key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres} />
-              ))}
-            </div>
-            )
-        }
-      </section>
-    );
-  }
+function App(){
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
-
 export default App;
---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => { 
+    return (
+        <div className='about'>
+            <h3>About this page . . .</h3>
+        </div>
+    );
+};
+
+export default About;
